4aafe2c67cba2b4306320ce710e3000fbb1cfced
Ujjwal <ujjwalmangalatreya@gmail.com>
2024-12-16 00:13:55 +0545

    contact up

--- lib/features/home_screen.dart/home_screen.dart
+++ lib/features/home_screen.dart/home_screen.dart
@@ -75,33 +75,56 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
               Container(
-                color: AppColors.homePageSecondSection,
-                width: MediaQuery.sizeOf(context).width,
-                child: Center(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 20,
+                color: AppColors.homePageFirstSection,
+                child: ClipPath(
+                  clipper: CustomShapeClipper(),
+                  child: Container(
+                    color: AppColors.homePageSecondSection,
+                    width: MediaQuery.sizeOf(context).width,
+                    child: Center(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text("Features Of School Management Software",
+                              style: AppFonts.homePageTitle),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(100, 10, 100, 0),
+                            child: Text(
+                              "eSkooly is a complete and feature-rich school management software for all educational institutes. This school management software is for learning, administration, and management activities in schools, colleges, universities, tuition centers, or training centers. Our free school management system manages everything starting from admission to attendance and exams to result in cards.",
+                              textAlign: TextAlign.center,
+                              softWrap: true,
+                              style: AppFonts.bodyText,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          FeatureGrid(),
+                        ],
                       ),
-                      Text("Features Of School Management Software",
-                          style: AppFonts.homePageTitle),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(100, 10, 100, 0),
-                        child: Text(
-                          "eSkooly is a complete and feature-rich school management software for all educational institutes. This school management software is for learning, administration, and management activities in schools, colleges, universities, tuition centers, or training centers. Our free school management system manages everything starting from admission to attendance and exams to result in cards.",
-                          textAlign: TextAlign.center,
-                          softWrap: true,
-                          style: AppFonts.bodyText,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 50,
-                      ),
-                      FeatureGrid(),
-                    ],
+                    ),
                   ),
                 ),
               ),
+              Container(
+                width: MediaQuery.sizeOf(context).width,
+                decoration: BoxDecoration(
+                  color: AppColors.homePageFirstSection,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "Drop Us a Line",
+                      style: AppFonts.homePageTitle,
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ));
