61eb2bd0f99b0e139fabb226453c8f9ec6997570
Ujjwal <ujjwalmangalatreya@gmail.com>
2024-12-17 20:01:00 +0545

    Home page and login page responsive

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+
 import 'package:get/get.dart';
 import 'package:schoolmgmt/core/widgets/responsive/responsive_layout.dart';
 import 'package:schoolmgmt/features/auth/views/mobile_login.dart';
