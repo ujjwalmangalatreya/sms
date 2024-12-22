072cb5ef771e054d3e35f98e1c5f8ee611053ca9
Ujjwal <ujjwalmangalatreya@gmail.com>
2024-12-22 18:28:19 +0545

    Add new employee layoy

--- lib/features/employee/views/add_employee.dart
+++ lib/features/employee/views/add_employee.dart
@@ -1,12 +1,77 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/constants/app_fonts.dart';
+
 class AddEmployee extends StatelessWidget {
   const AddEmployee({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("ADD EMPLOYEE"),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        //Header
+        Flexible(
+          flex: 1, // Adjust column size ratio
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Container(
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: const Color(0xFFF7E5E9), // Background color (#f7e5e9)
+                border: Border.all(
+                  color: const Color(0xFFF7E5E9), // Border color (#f7e5e9)
+                  width: 1.0, // Border width
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(40, 10, 0, 0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.settings, // Choose an appropriate icon
+                          color: Colors.black, // Set the icon color
+                          size: 24.0, // Set the icon size
+                        ),
+                        const SizedBox(
+                            width: 8.0), // Add space between icon and text
+                        Text(
+                          "EMPLOYEE",
+                          style: AppFonts.generalSettingsHeading,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(40, 10, 0, 0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.home, // Choose an appropriate icon
+                          color: Colors.black, // Set the icon color
+                          size: 24.0, // Set the icon size
+                        ),
+                        const SizedBox(
+                            width: 8.0), // Add space between icon and text
+                        Text(
+                          "All New",
+                          style: AppFonts.generalSettingsHeading,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        // OTHER CONTENT
+      ],
     );
   }
 }
